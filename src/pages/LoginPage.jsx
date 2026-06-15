296c95ca912b2ab14e28e6a906339f87decc837a
Shaik Jasmin <jasminshaik@msstechno.com>
2026-06-15 19:40:35 +0530

    some changes done

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -17,7 +17,7 @@ function Login() {
   const navigate = useNavigate();
 
   const validateEmail = (value) => {
-    const emailRegex = /^[a-zA-Z0-9._]+@msstechno\.com$/;
+    const emailRegex = /^[a-zA-Z0-9._]+@gmail\.com$/;
     return emailRegex.test(value);
   };
 
