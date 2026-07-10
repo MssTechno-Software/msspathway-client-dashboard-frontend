8f769ddc5b76a3d55256b6d603e2e5b7b2e9caa9
Shaik Jasmin <jasminshaik@msstechno.com>
2026-07-10 08:30:00 +0530

    prod repo fixed

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,4 @@
 const BASE_URL =
-  "https://uat-msspathway-software-backend-81057313575.asia-south1.run.app";
+  "https://timesheet-api-790373899641.asia-south1.run.app";
 
 export default BASE_URL;
